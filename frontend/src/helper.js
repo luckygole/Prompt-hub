0b658adf81bd461cadfa9a4b4bd46a0252ff467a
Lucky Gole <golelaksh@gmail.com>
2026-04-02 00:12:40 +0530

    feat: initial commit with updating projects

--- frontend/src/helper.js
+++ frontend/src/helper.js
@@ -1,1 +1,3 @@
-export const backendURL = import.meta.env.REACT_APP_API_URL || 'http://localhost:5000';
+// export const backendURL = import.meta.env.REACT_APP_API_URL || 'http://localhost:5000';
+
+export const backendURL = process.env.REACT_APP_API_URL;
