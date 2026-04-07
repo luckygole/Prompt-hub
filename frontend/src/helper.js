2c1e7c6e63afb46910a630290f7f3d3b7fe0e478
Lucky Gole <golelaksh@gmail.com>
2026-04-07 12:33:06 +0530

    feat: dockerize full-stack app with Docker Compose and env setup

--- frontend/src/helper.js
+++ frontend/src/helper.js
@@ -1,3 +1,6 @@
 // export const backendURL = import.meta.env.REACT_APP_API_URL || 'http://localhost:5000';
 
-export const backendURL = process.env.REACT_APP_API_URL;
+// export const backendURL = process.env.REACT_APP_API_URL;
+
+export const backendURL =
+  process.env.REACT_APP_API_URL || "http://localhost:5000";
